48fa08f3bd9c1097448b8533689964607db2a274
shadowcomer <kevinks.et@gmail.com>
2016-08-06 10:59:57 +0200

    Change setOption to receive the same test function

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -3,11 +3,12 @@ module Tests (tests) where
 import Distribution.TestSuite
 
 tests :: IO [Test]
-tests = return [ Test TestInstance
-                 { run = return $ Finished Pass
-                 , name = "succeeds"
-                 , tags = []
-                 , options = []
-                 , setOption = \_ _ -> Left "What?"
-                 }
-               ]
+tests = return [ Test succeeds ]
+  where
+    succeeds = TestInstance
+      { run = return $ Finished Pass
+      , name = "succeeds"
+      , tags = []
+      , options = []
+      , setOption = \_ _ -> Right succeeds
+      }
